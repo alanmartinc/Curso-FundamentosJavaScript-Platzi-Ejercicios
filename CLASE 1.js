198a27be1c635a9059cd798d57946a8311ec3403
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-25 22:25:21 -0300

    Se agrega ejercicio variables

---
+++ CLASE 1.js
@@ -0,0 +1,10 @@
+// console.log('Hola Mundo JS!')
+
+var nombre = 'Alan', apellido = 'Cabot'
+var edad = '22 años'
+var peso = 75
+
+// El punto y coma es opcional (;)
+
+console.log ('Hola ' + nombre + ' ' + apellido)
+console.log ('Tengo ' + edad)
