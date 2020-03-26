19ddc54f92ebcf966b63e93e1d744c35d0f342a1
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-25 22:31:18 -0300

    Se agrega ejercicio strings

---
+++ CLASE 2.js
@@ -0,0 +1,12 @@
+var nombre = 'Alan', apellido = 'Cabot'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+// var nombreCompleto = nombre + ' ' + apellido
+var nombreCompleto = `${nombre} ${apellido}`
+
+var str = nombre.substr(1, 2)
