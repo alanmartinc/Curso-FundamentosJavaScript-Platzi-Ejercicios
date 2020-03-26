565a378c3fe6576479b1e782718dc48f16e088e8
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-25 22:33:49 -0300

    Se agrega ejercicio variables y numeros

---
+++ CLASE 3.js
@@ -0,0 +1,24 @@
+var edad = 22
+// edad = edad + 1
+edad += 1
+
+var peso = 75
+// peso = peso - 2
+peso -= 2
+
+var sandwich = 1
+// peso = peso + sandwich
+peso += sandwich
+
+var jugarAlFutbol = 3
+// peso = peso - jugarAlFutbol
+peso -= jugarAlFutbol
+
+var precioDeVino = 200.3
+var total = Math.round(precioDeVino * 100 * 3) / 100
+var totalStr = total.toFixed(2)
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+var personas = 2
+var cantidadDePorcionesPorPersonas = pizza / personas
